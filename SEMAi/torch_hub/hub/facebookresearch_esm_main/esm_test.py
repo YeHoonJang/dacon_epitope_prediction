46ed17214f3c1b7ba95c0d0aa3647434edc18329
YeHoon Jang <jangyh0420@gmail.com>
2022-07-20 02:19:33 +0900

    train size issue...

--- SEMAi/torch_hub/hub/facebookresearch_esm_main/esm_test.py
+++ SEMAi/torch_hub/hub/facebookresearch_esm_main/esm_test.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset, DataLoader
 import transformers
 from transformers.modeling_outputs import SequenceClassifierOutput
 from transformers import Trainer
+from sklearn.metrics import f1_score
 
 import esm
 
@@ -12,6 +13,7 @@ import pdb
 import tqdm
 import pickle
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -23,14 +25,17 @@ class ESMClassification(nn.Module):
 
         self.model, self.alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
         self.classifier = nn.Linear(1280, self.num_labels)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, token_ids):
         outputs = self.model.forward(token_ids, repr_layers=[33])['representations'][33]
-        outputs = outputs[:, 1:-1, :]
+        outputs = outputs[:, :, :]
         logits = self.classifier(outputs)
+        pdb.set_trace()
+        logits = self.sigmoid(logits)
 
-        return SequenceClassifierOutput(logits=logits)
-
+        # return SequenceClassifierOutput(logits=logits)
+        return logits
 
 class MaskedMSELoss(torch.nn.Module):
     def __init__(self):
@@ -57,8 +62,9 @@ def train_epoch(model, data_loader, criterion, optimizer, device):
         for _, (x, y) in enumerate(data_loader):
             x = x.to(device)
             y = y.to(device)
-
+            pdb.set_trace()
             output = model(x)
+            output = torch.topk(output, 1)
             loss = criterion(output, y)
             loss_value = loss.item()
             train_loss += loss_value
@@ -91,15 +97,15 @@ def evaluate(model, data_loader, criterion, device):
 
             pbar.update(1)
 
-    return valid_loss / total
+    return valid_loss / total, y, output
 
 
 class CustomDataset(Dataset):
     # 반드시 init, len, getitem 구현
-    def __init__(self, data):
+    def __init__(self, data, alphabet):
         # 빈 리스트 생성 <- 데이터 저장
         # TODO 여기부터
-        self.X = data['epitope_seq']    # sequence
+        self.X = data['epitope_seq'].str.split('').apply(lambda x:[alphabet.get_idx(x[i]) for i in range(1, len(x)-1)]).apply(lambda x: x[:14]).apply(lambda x: np.pad(x, (0,14-len(x)), 'constant', constant_values=(1)))  # sequence
         self.y = data['label']          # label
 
     def __len__(self):
@@ -112,7 +118,7 @@ class CustomDataset(Dataset):
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'Initializing Device: {device}')
 
-# Load ESM-1b model
+_, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
 
 # batch_converter = alphabet.get_batch_converter()
 
@@ -135,34 +141,52 @@ valid_data = data.iloc[int(len(data)*0.8):, :]
 #     test_data = pickle.load(handle)
 pdb.set_trace()
 
+train_data = CustomDataset(train_data, alphabet)
+valid_data = CustomDataset(valid_data, alphabet)
+
 # train_batch_labels, train_batch_strs, train_batch_tokens = batch_converter(train_data)
 # test_batch_labels, test_batch_strs, test_batch_tokens = batch_converter(test_data)
-pdb.set_trace()
-
-model = ESMClassification()
-_, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
 pdb.set_trace()
 
 batch_size = 16
 train_loader = DataLoader(train_data, batch_size=batch_size, drop_last=True, num_workers=8)    # drop_last = drop the last incomplete batch
 valid_loader = DataLoader(valid_data, batch_size=batch_size, drop_last=True, num_workers=8)
 
+model = ESMClassification().to(device)
+criterion = nn.MSELoss()
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
 
-# with torch.no_grad():
-results = model(train_batch_tokens, repr_layers=[33], return_contacts=True).cuda()
-token_representations = results["representations"][33]
-pdb.set_trace()
+start_epoch = 0
+epochs = 10
+print("Start Training...!")
+for epoch in range(start_epoch, epochs):
+    print(f"Epoch: {epoch}")
+    train_loss = train_epoch(model, train_loader, criterion, optimizer, device)
+    valid_loss, label, pred = evaluate(model, valid_loader, criterion, device)
 
-# Generate per-sequence representations via averaging
-# NOTE: token 0 is always a beginning-of-sequence token, so the first residue is token 1.
-sequence_representations = []
-for i, (_, seq) in enumerate(train_data):
-    sequence_representations.append(token_representations[i, 1 : len(seq) + 1].mean(0))
-pdb.set_trace()
+    valid_f1 = f1_score(label, pred, average='macro')
 
-# Look at the unsupervised self-attention map contact predictions
+    print(f"Training Loss: {train_loss:.5f}")
+    print(f"Validation Loss: {valid_loss:.5f}")
+    print(f"F1 Score: {valid_f1:.5f}")
 
-for (_, seq), attention_contacts in zip(test_data, results["contacts"]):
-    plt.matshow(attention_contacts[: len(seq), : len(seq)])
-    plt.title(seq)
-    plt.show()
+
+
+# # with torch.no_grad():
+# results = model(train_batch_tokens, repr_layers=[33], return_contacts=True).cuda()
+# token_representations = results["representations"][33]
+# pdb.set_trace()
+#
+# # Generate per-sequence representations via averaging
+# # NOTE: token 0 is always a beginning-of-sequence token, so the first residue is token 1.
+# sequence_representations = []
+# for i, (_, seq) in enumerate(train_data):
+#     sequence_representations.append(token_representations[i, 1 : len(seq) + 1].mean(0))
+# pdb.set_trace()
+#
+# # Look at the unsupervised self-attention map contact predictions
+#
+# for (_, seq), attention_contacts in zip(test_data, results["contacts"]):
+#     plt.matshow(attention_contacts[: len(seq), : len(seq)])
+#     plt.title(seq)
+#     plt.show()
